a8b76862f11cea1bc17314cd9644c1cdefab6156
itgitnit <Thakurnikhil2210@gmail.com>
2021-11-09 00:09:22 +0530

    Updates

---
+++ precache-manifest.58d0a67ec42d283347a05b4fbe00ca25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9590d65abb4d9f5fe4b9b6346d03df2c",
+    "url": "/textUtils/index.html"
+  },
+  {
+    "revision": "4bc96bf1fa7c8073cda0",
+    "url": "/textUtils/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "be00779ae91b3fc99eae",
+    "url": "/textUtils/static/js/2.5ea02daf.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/textUtils/static/js/2.5ea02daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc96bf1fa7c8073cda0",
+    "url": "/textUtils/static/js/main.146cc7a2.chunk.js"
+  },
+  {
+    "revision": "2632919577b9e88b80ad",
+    "url": "/textUtils/static/js/runtime-main.387ecd21.js"
+  }
+]);
